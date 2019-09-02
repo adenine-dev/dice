a6237557b26578af41e0b27f402e7345fd267ae7
alexa <alexa.griffin42@gmail.com>
2019-09-02 18:46:58 -0400

    styles and more functionality

--- script/index.js
+++ script/index.js
@@ -4,25 +4,30 @@
     for(var i = 0; i < Object.keys(attribs).length; i++) {
       node[Object.keys(attribs)[i]] = Object.values(attribs)[i]
     }
+
     for (var i = 0; i < children.length; i++) {
       if (typeof children[i] != "string") node.appendChild(children[i])
       else node.appendChild(document.createTextNode(children[i]))
     }
+    
     return node
   }
 
   function die(n) {
     var history = []
-    document.body.appendChild(elt("div", { 
-      className: "die"
+    var historyEl = elt("p", {}, "history")
+    document.getElementById("root").appendChild(elt("div", { 
+      className: "die d" + n
     }, 
       elt("h1", {  }, "d" + n),
       elt("button", {
         onclick: function(e) {
           history.push(Math.floor(Math.random() * n) + 1)
           e.target.innerText = history[history.length - 1]
+          historyEl.innerText = history.slice(Math.max(0, history.length - 20), history.length - 1).join(", ") || "history"
         }
-      }, "oof")
+      }, "?"),
+      historyEl
     ))
   }
 
