bcbbf869cfc77ae5453a6458c3ed7b252dfeae8f
alexa <alexa.griffin42@gmail.com>
2019-09-02 18:12:04 -0400

    more boilerplate stuffs

--- script/index.js
+++ script/index.js
@@ -1,3 +1,17 @@
 (function() {
-  console.log("oof")
+  function elt(type, attribs, ...children) {
+    var node = document.createElement(type)
+    for(var i = 0; i < Object.keys(attribs).length; i++) {
+      node[Object.keys(attribs)[i]] = Object.values(attribs)[i]
+    }
+    for (var i = 0; i < children.length; i++) {
+      if (typeof children[i] != "string") node.appendChild(children[i])
+      else node.appendChild(document.createTextNode(children[i]))
+    }
+    return node
+  }
+
+  document.body.appendChild(elt("h1", { 
+    className: "oof"
+  }, "oof"))
 })()
