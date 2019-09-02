d69934d008b9fd27d27dc721f40ac67a26e35827
alexa <alexa.griffin42@gmail.com>
2019-09-02 18:20:39 -0400

    basic functionality

--- script/index.js
+++ script/index.js
@@ -11,7 +11,25 @@
     return node
   }
 
-  document.body.appendChild(elt("h1", { 
-    className: "oof"
-  }, "oof"))
+  function die(n) {
+    var history = []
+    document.body.appendChild(elt("div", { 
+      className: "die"
+    }, 
+      elt("h1", {  }, "d" + n),
+      elt("button", {
+        onclick: function(e) {
+          history.push(Math.floor(Math.random() * n) + 1)
+          e.target.innerText = history[history.length - 1]
+        }
+      }, "oof")
+    ))
+  }
+
+  die(4)
+  die(6)
+  die(8)
+  die(10)
+  die(12)
+  die(20)
 })()
